58baed56f498f3cf942d4bcb9e8b31143acc3d31
Rafael <rafasantosalves2013@gmail.com>
2023-12-02 15:22:17 -0300

    Versão melhorada

--- hanoi.c
+++ hanoi.c
@@ -3,21 +3,6 @@
 int discos;
 int **torres;
 int total;
-void inserir(int disco,int* torreIn, int* torreFi){
-    int i,j;
-    for(i =0;i<discos;i++){
-        if (torreIn[i] == disco){
-            for(j = 0; j<discos;j++){
-                if (torreFi[j] == 0){
-                    torreFi[j] = disco;
-                    torreIn[i] = 0;
-                    break;
-                }
-            }
-        }
-    }
-}
-
 int procurarDisco(int disco, int** torres){
     int i, j;
     for(i=0;i<3;i++){
@@ -28,6 +13,23 @@ int procurarDisco(int disco, int** torres){
         }
     }
 }
+void inserir(int disco,int* torreIn, int* torreFi){
+    int i,j;
+    for(i =0;i<discos;i++){
+        if (torreIn[i] == disco){
+            for(j = 0; j<discos;j++){
+                if (torreFi[j] == 0){
+                    torreFi[j] = disco;
+                    torreIn[i] = 0;
+                    printf("Passando o disco %d para a Torre %c\n",disco,(procurarDisco(disco,torres))%3+65);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
 void hanoi(int discos, int** torres,int total){
     if(discos == 2 && total % 2 == 1){
         inserir(1, torres[procurarDisco(1,torres)],torres[(procurarDisco(1,torres)+2)%3]);
@@ -39,32 +41,16 @@ void hanoi(int discos, int** torres,int total){
         inserir(2,torres[procurarDisco(2,torres)],torres[(procurarDisco(2,torres)+2)%3]);
         inserir(1, torres[procurarDisco(1,torres)],torres[((procurarDisco(1,torres)+1))%3]);
     }
-    if(discos != total && total % 2 == 1 && discos != 2 && discos % 2 == 1){
+    if(total % 2 == discos % 2 && discos != 2){
         hanoi(discos-1,torres,total);
         inserir(discos, torres[procurarDisco(discos,torres)],torres[(procurarDisco(discos,torres)+2)%3]);
         hanoi(discos-1,torres,total);
     }
-    if(discos != total && total % 2 == 1 && discos != 2 && discos % 2 == 0){
+    if(total % 2 != discos % 2  && discos != 2){
         hanoi(discos-1,torres,total);
         inserir(discos, torres[procurarDisco(discos,torres)],torres[(procurarDisco(discos,torres)+1)%3]);
         hanoi(discos-1,torres,total);
     }
-    if(discos != total && total % 2 == 0 && discos != 2 && discos % 2 == 1){
-        hanoi(discos-1,torres,total);
-        inserir(discos, torres[procurarDisco(discos,torres)],torres[(procurarDisco(discos,torres)+1)%3]);
-        hanoi(discos-1,torres,total);
-    }
-    if(discos != total && total % 2 == 0 && discos != 2 && discos % 2 == 0){
-        hanoi(discos-1,torres,total);
-        inserir(discos, torres[procurarDisco(discos,torres)],torres[(procurarDisco(discos,torres)+2)%3]);
-        hanoi(discos-1,torres,total);
-    }
-    if(discos == total && total != 2){
-        hanoi(discos-1,torres,total);
-        inserir(discos, torres[procurarDisco(discos,torres)],torres[(procurarDisco(discos,torres)+2)%3]);
-        hanoi(discos-1,torres,total);
-    }
-
 }
 int main(){
    
@@ -82,7 +68,7 @@ int main(){
     }
     hanoi(discos,torres,total);
 
-   for(i = 0; i <= discos; i++){
+   /*for(i = 0; i <= discos; i++){
         printf("Torre 1 %d\t Torre 2 %d\t Torre 3 %d\t\n",torres[0][discos - i],torres[1][discos - i],torres[2][discos - i]);
-    }
+    }*/
 }
